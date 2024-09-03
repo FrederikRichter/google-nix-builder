0bdb7ef160fae5fdcda19b204ff6aa301e6d954d
FrederikRichter <Frederik.Richter@mailbox.org>
2024-09-03 19:35:20 +0200

    Dockerfile is injected with ssh and build on google servers

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "logging into gcloud"
+gcloud auth login
+read -p "Enter project id: " project_id
+echo "setting gcloud project"
+gcloud config set project "$project_id"
+echo "generating ssh keys"
+ssh-keygen -t rsa -f ./cloudbuild_ssh -C "$USER" -N ''
+echo "generating Dockerfile from template"
+cp Dockerfile_template Dockerfile
+echo "copying ssh public key to project"
+sed -i "s|SSH_KEY_PLACEHOLDER|$(cat ./cloudbuild_ssh.pub | sed 's|[/&]|\\&|g')|g" Dockerfile
+echo "building dockerfile on google build"
+gcloud builds submit --config cloudbuild.yaml .
+echo "deleting Dockerfile"
+rm Dockerfile
--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ google-cloud-sdk ];
+    nativeBuildInputs = with pkgs.buildPackages; [ google-cloud-sdk bash ];
 }
