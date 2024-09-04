428994c01389cc6ce1b9109f56a80d5a81fcef57
Frederik Richter <frederik.richter@mailbox.org>
2024-09-04 15:18:45 +0200

    build process not working anymore for some reason

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# BUILD PROCESS
 echo "removing Dockerfile from .gitignore"
 sed -i '/^Dockerfile$/d' .gitignore
 echo "logging into gcloud"
@@ -6,6 +8,8 @@ gcloud auth login
 read -p "Enter project id: " project_id
 echo "setting gcloud project"
 gcloud config set project "$project_id"
+echo "getting region"
+read -p "Enter region: " region
 echo "generating ssh keys"
 ssh-keygen -t rsa -f ./cloudbuild_ssh -C "$USER" -N ''
 echo "generating Dockerfile from template"
@@ -13,8 +17,19 @@ cp Dockerfile_template Dockerfile
 echo "copying ssh public key to project"
 sed -i "s|SSH_KEY_PLACEHOLDER|$(cat ./cloudbuild_ssh.pub | sed 's|[/&]|\\&|g')|g" Dockerfile
 echo "building dockerfile on google build"
-gcloud builds submit --config cloudbuild.yaml .
+gcloud builds submit --region="$region" --config cloudbuild.yaml .
 echo "deleting Dockerfile"
 rm Dockerfile
 echo "appending Dockerfile to .gitignore"
 echo "Dockerfile" >> .gitignore
+
+# RUN PROCESS
+echo "creating buckets"
+gcloud storage buckets create gs://nix-remote-build-cache --location="$region"
+echo "running container"
+gcloud run deploy nix-remote-build-container \
+  --image 'gcr.io/nix-remote-build/nix-remote-builder:latest' \
+  --region $region \
+  --execution-environment=gen2 \
+  --add-volume=name=build_cache,type=cloud-storage,bucket=nix-remote-build-cache \
+  --add-volume-mount=volume=build_cache,mount-path=/mnt/build-cache/
